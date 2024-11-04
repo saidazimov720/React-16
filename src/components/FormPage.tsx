b891a6c8e0bc8db15ce3309ed55185c1d64d18dc
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-04 11:54:48 +0500

    textfield

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -34,3 +34,26 @@ const FormPage = () => {
             state: {formData}
         });
     };
+
+    
+    return(
+        <Container maxWidth="sm">
+            <Box mt={5}>
+                <Typography variant="h4" > Visa Form</Typography>
+                <form onSubmit={handleSubmit}> 
+                <TextField label="First name"
+                     name="First name"
+                      value={formData.firstName}
+                       onChange={handleChange} 
+                       fullWidth 
+                       required
+                       margin="dense"
+                       />
+                
+                 </form>
+            </Box>
+        </Container>
+    );
+};
+
+export default FormPage;
