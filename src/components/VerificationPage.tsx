116fefb9cb49363eaa3055248a243b6c2a3006dc
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-04 12:00:31 +0500

    create the verificationPage interface

--- src/components/VerificationPage.tsx
+++ src/components/VerificationPage.tsx
@@ -1,2 +1,15 @@
 import React from "react";
 import { Container, Typography, Button, Box } from "@mui/material";
+
+interface VerificationPage{
+    location: {
+        state:{
+            formData: {
+                firstName: string;
+                lastName: string;
+                email: string;
+                passportNumber: string;
+            }
+        }
+    }
+}
